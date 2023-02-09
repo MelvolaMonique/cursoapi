c360cb99e20f35134080a4e6c9dccb8fd07e9969
MelvolaMonique <monique.lima10@etec.gov.br>
2023-02-09 10:51:33 -0300

    TCC 1 parte-criação

---
+++ src/test/java/br/com/etec/monique/cursoapi/CursoapiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.etec.monique.cursoapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursoapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
