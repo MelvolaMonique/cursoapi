256f91bf700703fc8d0ea02178ba8d8eb352db49
MelvolaMonique <monique.lima10@etec.gov.br>
2023-06-22 08:32:18 -0300

    olocando os ultimos filtros para o aluno
    colocando filtros e arrumando o resource e testando

--- src/main/java/br/com/etec/monique/cursoapi/repository/filter/aluno/AlunoRepositoryImpl.java
+++ src/main/java/br/com/etec/monique/cursoapi/repository/filter/aluno/AlunoRepositoryImpl.java
@@ -1,15 +1,13 @@
 package br.com.etec.monique.cursoapi.repository.filter.aluno;
 
 import br.com.etec.monique.cursoapi.model.Aluno;
-import br.com.etec.monique.cursoapi.model.Curso;
 import br.com.etec.monique.cursoapi.repository.filter.AlunoFilter;
-import br.com.etec.monique.cursoapi.repository.filter.CursoFilter;
 import br.com.etec.monique.cursoapi.repository.projections.AlunoDto;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
 
-import javax.persistence.Entity;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.persistence.TypedQuery;
@@ -17,7 +15,9 @@ import javax.persistence.criteria.CriteriaBuilder;
 import javax.persistence.criteria.CriteriaQuery;
 import javax.persistence.criteria.Predicate;
 import javax.persistence.criteria.Root;
-import java.lang.reflect.Type;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class AlunoRepositoryImpl implements AlunoRepositoryQuery {
 
@@ -25,19 +25,18 @@ public class AlunoRepositoryImpl implements AlunoRepositoryQuery {
   private EntityManager manager;
 
   @Override
-  public Page<AlunoDto> filtrar(AlunoFilter alunoFilter, Pageable pageable){
+  public Page<AlunoDto> filtrar(AlunoFilter alunoFilter, Pageable pageable) {
 
     CriteriaBuilder builder = manager.getCriteriaBuilder();
     CriteriaQuery<AlunoDto> criteria = builder.createQuery(AlunoDto.class);
     Root<Aluno> root = criteria.from(Aluno.class);
 
     criteria.select(builder.construct(AlunoDto.class
-      ,root.get("id")
-      ,root.get("nomealuno")
-      ,root.get("nomecidade")
-      ,root.get("cidade").get("nomecidade")
-      ,root.get("cidade").get("uf")
-      ,root.get("curso").get("nomecurso")
+      , root.get("id")
+      , root.get("nomealuno")
+      , root.get("cidade").get("nomecidade")
+      , root.get("cidade").get("uf")
+      , root.get("curso").get("nomecurso")
     ));
 
     Predicate[] predicates = criarRestricoes(alunoFilter, builder, root);
@@ -48,7 +47,33 @@ public class AlunoRepositoryImpl implements AlunoRepositoryQuery {
     adicionarRestricoesDePaginacao(query, pageable);
 
     return new PageImpl<>(query.getResultList(), pageable, total(alunoFilter));
-    
+
+  }
+
+  private Predicate[] criarRestricoes(AlunoFilter alunoFilter, CriteriaBuilder builder, Root<Aluno> root) {
+    List<Predicate> predicates = new ArrayList<>();
+
+    if (!StringUtils.isEmpty(alunoFilter.getNomealuno())) {
+      predicates.add(builder.like(builder.lower(root.get("nomealuno")),
+        "%" + alunoFilter.getNomecidade().toLowerCase() + "%"));
+
+    }
+    if (!StringUtils.isEmpty(alunoFilter.getNomecidade())) {
+      predicates.add(builder.like(builder.lower(root.get("cidade").get("nomecidade")),
+        "%" + alunoFilter.getNomecidade().toLowerCase() + "%"));
+
+    }
+    if (!StringUtils.isEmpty(alunoFilter.getUf())) {
+      predicates.add(builder.equal(builder.lower(root.get("cidade").get("uf")),
+        alunoFilter.getUf().toLowerCase()));
+
+    }
+    if (!StringUtils.isEmpty(alunoFilter.getNomecurso())) {
+      predicates.add(builder.like(builder.lower(root.get("curso").get("nomecurso")),
+        "%" + alunoFilter.getNomecurso().toLowerCase() + "%"));
+
+    }
+    return predicates.toArray(new Predicate[predicates.size()]);
   }
 
   private void adicionarRestricoesDePaginacao(TypedQuery<AlunoDto> query, Pageable pageable) {
@@ -76,4 +101,4 @@ public class AlunoRepositoryImpl implements AlunoRepositoryQuery {
 }
 
 
-}
+
---
+++ src/main/java/br/com/etec/monique/cursoapi/resource/AlunoResource.java
@@ -0,0 +1,33 @@
+package br.com.etec.monique.cursoapi.resource;
+
+import br.com.etec.monique.cursoapi.model.Aluno;
+import br.com.etec.monique.cursoapi.repository.AlunoRepository;
+import br.com.etec.monique.cursoapi.repository.filter.AlunoFilter;
+import br.com.etec.monique.cursoapi.repository.projections.AlunoDto;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/alunos")
+
+
+public class AlunoResource {
+
+  @Autowired
+  private AlunoRepository alunoRepository;
+
+  @GetMapping("/todos")
+  public List<Aluno> listarTodosAlunos(){ return alunoRepository.findAll(); }
+
+@GetMapping()
+public Page<AlunoDto> pesquisar (AlunoFilter alunoFilter, Pageable pageable){
+    return alunoRepository.filtrar(alunoFilter, pageable);
+}
+
+}
